0199bb5e6fa57ff7bca0ba628d105314ea1dbfdc
Duongatun19 <95019544+Duongatun19@users.noreply.github.com>
2022-11-16 03:21:30 +0700

    update

--- tuan.js
+++ tuan.js
@@ -1,10 +1,10 @@
 let menu = document.getElementById('menu');
-document.addEventListener('contextmenu',function(event){
+document.addEventListener('contextmenu', function (event) {
     event.preventDefault();
     menu.style.display = 'block';
     menu.style.top = event.y + 'px';
-    menu.style.left = event.x +'px';
+    menu.style.left = event.x + 'px';
 })
-document.addEventListener('click',function(){
+document.addEventListener('click', function () {
     menu.style.display = 'none';
 })
