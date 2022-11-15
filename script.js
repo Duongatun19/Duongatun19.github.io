526c94185406c0aa4b221acdc5f848492c28281f
Duongatun19 <95019544+Duongatun19@users.noreply.github.com>
2022-11-16 02:33:37 +0700

    Add file script

---
+++ script.js
@@ -0,0 +1,10 @@
+let menu = document.getElementById('menu');
+document.addEventListener('contextmenu',function(event){
+    event.preventDefault();
+    menu.style.display = 'block';
+    menu.style.top = event.y + 'px';
+    menu.style.left = event.x +'px';
+})
+document.addEventListener('click',function(){
+    menu.style.display = 'none';
+})
